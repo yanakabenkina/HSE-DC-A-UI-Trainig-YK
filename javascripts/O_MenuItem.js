0a24f40c06dc2cc26535433080ab90e9c539787c
yanakabenkina <31996370+yanakabenkina@users.noreply.github.com>
2018-04-14 17:08:48 +0300

    add transition

--- javascripts/O_MenuItem.js
+++ javascripts/O_MenuItem.js
@@ -1,13 +1,34 @@
 $(function() {
   $('.A_MenuItemButton').click(function(e) {
-  let container = $(e.target).closest('.O_MenuItem');
+    e.preventDefault();
 
-  if (container.hasClass('active')) {
-    container.removeClass('active');
-  } else {
-    $('.O_MenuItem').removeClass('active');
-    container.addClass('active');
-  }
+    let container = $(e.target).closest('.O_MenuItem');
 
+    let subItems = container.find('.A_SubmenuItem');
+
+    let item = subItems[0];
+
+    let height = 0;
+
+    if (item) {
+      let itemHeight = $(item).height();
+      height = subItems.length * itemHeight;
+    };
+
+    container.find('.M_Submenu').height(height);
+
+
+    if (container.hasClass('active')) {
+      container.removeClass('active');
+
+      container.find('.M_Submenu').height(0);
+    } else {
+      $('.O_MenuItem').removeClass('active');
+      $('.M_Submenu').height(0);
+      
+      container.addClass('active');
+
+      container.find('.M_Submenu').height(height);
+    }
   });
 });
